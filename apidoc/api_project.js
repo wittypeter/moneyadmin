0fe81a4b0b0f0562a88d16bc2f4e8f64598c8bf5
Peter Han <hanzp8023@163.com>
2018-12-18 22:45:38 +0800

    feat: integrate apidoc

---
+++ apidoc/api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "test APIS",
+  "version": "1.0.0",
+  "description": "test API",
+  "title": "test apis title",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-12-17T16:15:05.104Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
